9669787bdb231a7b3e830af1ece11ec827926d70
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-21 13:18:46 -0700

    Finished Mapping function practice

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+//renders one individual card for an emoji entry
+function Card(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {props.emoji}
+        </span>
+        <span>{props.name}</span>
+      </dt>
+      <dd>{props.meaning}</dd>
+    </div>
+  );
+}
+
+export default Card;
